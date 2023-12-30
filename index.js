fd118eb06760aaeab0a407a3285ac246f696614e
Djenntt <38169617+djenntt@users.noreply.github.com>
2023-12-29 20:04:08 -0700

    better error handling & reporting
    this allows multiple retries for downloading each image (total of 3 attempts), and if completely fails it is added to a report once complete, the numbering sequence is retained

--- index.js
+++ index.js
@@ -5,23 +5,32 @@ const path = require('path');
 const { exec } = require('child_process');
 const rimraf = require('rimraf');
 
-async function downloadImage(imageUrl, folderPath) {
+async function downloadImage(imageUrl, folderPath, maxRetries = 1) {
     const imageName = path.basename(new URL(imageUrl).pathname);
     const imagePath = path.resolve(__dirname, folderPath, imageName);
-    const writer = fs.createWriteStream(imagePath);
 
-    const response = await axios({
-        url: imageUrl,
-        method: 'GET',
-        responseType: 'stream'
-    });
-
-    response.data.pipe(writer);
-
-    return new Promise((resolve, reject) => {
-        writer.on('finish', resolve);
-        writer.on('error', reject);
-    });
+    for (let attempt = 1; attempt <= maxRetries + 1; attempt++) {
+        try {
+            const writer = fs.createWriteStream(imagePath);
+            const response = await axios({
+                url: imageUrl,
+                method: 'GET',
+                responseType: 'stream'
+            });
+            response.data.pipe(writer);
+            return new Promise((resolve, reject) => {
+                writer.on('finish', resolve);
+                writer.on('error', () => {
+                    writer.close();
+                    reject(new Error(`Failed to download image on attempt ${attempt}`));
+                });
+            });
+        } catch (error) {
+            if (attempt === maxRetries + 1) {
+                return Promise.reject(error);
+            }
+        }
+    }
 }
 
 function formatSequenceNumber(number, totalUrls) {
@@ -30,16 +39,16 @@ function formatSequenceNumber(number, totalUrls) {
 }
 
 (async () => {
+    const errors = [];
+    let pdfSequenceNumber = 1;
     const urls = fs.readFileSync('urls.txt', 'utf8').split('\n').filter(Boolean);
     const basePdfName = process.argv[2] || 'output';
     const mode = process.argv[3] || '';
     const cssSelector = process.argv[4] || 'img';
 
     for (let i = 0; i < urls.length; i++) {
+        let failed = false;
         const url = urls[i];
-        const sequenceNumber = formatSequenceNumber(i + 1, urls.length);
-        const pdfName = `${basePdfName}_${sequenceNumber}`;
-
         const browser = await puppeteer.launch({ headless: 'new' });
         const page = await browser.newPage();
         await page.goto(url, { waitUntil: 'networkidle2' });
@@ -56,44 +65,55 @@ function formatSequenceNumber(number, totalUrls) {
             fs.mkdirSync(folderPath, { recursive: true });
         }
 
-        const imagesFolderPath = path.join('./images', pdfName);
+        const imagesFolderPath = path.join(folderPath, `images_${pdfSequenceNumber}`);
         if (!fs.existsSync(imagesFolderPath)) {
             fs.mkdirSync(imagesFolderPath, { recursive: true });
         }
 
-        const downloadPromises = imageUrls.map(imageUrl => downloadImage(imageUrl, imagesFolderPath));
-        await Promise.all(downloadPromises);
+        const downloadPromises = imageUrls.map(imageUrl => downloadImage(imageUrl, imagesFolderPath, 2));
+        const results = await Promise.allSettled(downloadPromises);
 
-        console.log(`All images from ${url} have been downloaded.`);
-
-        if (!/pdf/i.test(mode)) {
-            return;
-        }
-
-        const pdfFolderPath = path.join('./', basePdfName);
-        if (!fs.existsSync(pdfFolderPath)) {
-            fs.mkdirSync(pdfFolderPath, { recursive: true });
-        }
-        const pdfPath = path.join(pdfFolderPath, `${pdfName}.pdf`);
-
-        const convertCommand = `magick convert ${imagesFolderPath}/*.* ${pdfPath}`;
-
-        exec(convertCommand, (error, _stdout, stderr) => {
-            if (error) {
-                console.error(`Error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-                return;
-            }
-
-            console.log(`Images converted to PDF: ${pdfPath}`);
-
-            if (mode === 'pdfclean') {
-                rimraf.sync(imagesFolderPath);
-                console.log('Images folder removed.');
+        results.forEach((result, index) => {
+            if (result.status === 'rejected') {
+                failed = true;
+                errors.push({ url: imageUrls[index], error: result.reason.message });
             }
         });
+
+        if (errors.length === 0) {
+            const formattedSequenceNumber = formatSequenceNumber(pdfSequenceNumber, urls.length);
+            const pdfName = `${basePdfName}_${formattedSequenceNumber}`;
+            const pdfFolderPath = path.join('./pdfs', basePdfName);
+
+            if (!fs.existsSync(pdfFolderPath)) {
+                fs.mkdirSync(pdfFolderPath, { recursive: true });
+            }
+
+            const pdfPath = path.join(pdfFolderPath, `${pdfName}.pdf`);
+            const convertCommand = `magick convert ${imagesFolderPath}/*.* ${pdfPath}`;
+
+            exec(convertCommand, (error, _stdout, stderr) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    return;
+                }
+
+                console.log(`Images converted to PDF: ${pdfPath}`);
+                if (mode === 'pdfclean') {
+                    rimraf.sync(imagesFolderPath);
+                    console.log('Images folder removed.');
+                }
+            });
+        }
+
+        pdfSequenceNumber++;
+    }
+
+    if (errors.length > 0) {
+        console.error(`Errors occurred:`, errors);
     }
 })();
